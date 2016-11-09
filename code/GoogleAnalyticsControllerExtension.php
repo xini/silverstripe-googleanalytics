58ad455d4b6019ebc1673d158810f139cfa84388
Florian Thoma <florian.thoma@innoweb.com.au>
2016-11-10 09:50:36 +1100

    fix id implementation

--- code/GoogleAnalyticsControllerExtension.php
+++ code/GoogleAnalyticsControllerExtension.php
@@ -186,13 +186,13 @@ class GoogleAnalyticsControllerExtension extends Extension
                     
                     // tracking code
                     Requirements::insertHeadTags("
-	<noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=".$config.GoogleAnalyticsID."\"
+	<noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=".$config->GoogleAnalyticsID."\"
 	height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','".$config.GoogleAnalyticsID."');</script>
+	})(window,document,'script','dataLayer','".$config->GoogleAnalyticsID."');</script>
 					");
                 }
                 
